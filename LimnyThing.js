2081f80ffdad53f6ddb17d65a7df5c26ba236020
Borton Fweems <borton.fweems@treadleandloam.com>
2023-01-26 17:07:44 -0800

    put some flesh on them LimnyThing/SpaceThing bones.

--- LimnyThing.js
+++ LimnyThing.js
@@ -13,6 +13,13 @@ import { Limnable } from "./Limnable.js"
 export class LimnyThing
        extends base_class (PouchyThing) . and_interfaces (Limnable)
 { //
+  static breather_of_record = null;
+  static BreatherOfRecord ()
+    { return this.breather_of_record; }
+  static SetBreatherOfRecord (lung)
+    { this.breather_of_record = lung; }
+
+  //
   constructor ()
     { super ();
       this.limn_flags = 0;
@@ -20,4 +27,9 @@ export class LimnyThing
       this.adj_iro = null;
       this.cumu_adjc = null;
     }
+
+  CurrentCumuMats ()
+    { return this.cumu_mats; }
+  DependCumuMatsFrom (cm_above)
+    { return (this.cumu_mats = cm_above); }
 }
--- SpaceThing.js
+++ SpaceThing.js
@@ -6,7 +6,9 @@
 
 import { Vect } from "./Vect.js";
 
-import { LimnyThing } from "./LimnyThing.js"
+import { GrapplerPile } from "./GrapplerPile.js";
+
+import { LimnyThing } from "./LimnyThing.js";
 
 
 export class SpaceThing  extends LimnyThing
@@ -14,5 +16,55 @@ export class SpaceThing  extends LimnyThing
   constructor ()
     { super ();
       this.gr_pile = null;
+    }
+
+  UnsecuredGrapplerPile ()
+    { return this.gr_pile; }
+  AssuredGrapplerPile ()
+    { if (this.gr_pile == null)
+        this.gr_pile = new GrapplerPile ();
+      return this.gr_pile;
+    }
+
+  AppendGrappler (g)
+    { if (g != null)
+        this.AssuredGrapplerPile () . AppendGrappler (g);
+      return this;
+    }
+
+  PrependGrappler (g)
+    { if (g != null)
+        this.AssuredGrapplerPile () . PrependGrappler (g);
+      return this;
+    }
+
+  FindGrappler (nm)
+    { let gp = this.UnsecuredGrapplerPile ();
+      return (gp != null)  ?  gp . FindGrappler (nm)  :  null;
+    }
+
+  DependCumuMatsFrom (cm_above)
+    { if (this.gr_pile == null)
+        return (this.cumu_mats = cm_above);
+      if (this.cumu_mats == null)
+        this.cumu_mats = new CumuMats (false);
+      if (cm_above == null)
+        this.cumu_mats . FreshenVia (this.gr_pile.pnt_mat,
+                                     this.gr_pile.nrm_mat,
+                                     this.gr_pile.inv_pnt_mat,
+                                     this.gr_pile.inv_nrm_mat);
+      else
+          this.cumu_mats
+            . FreshenVia (this.gr_pile.pnt_mat . Mul (cm_above.pmat),
+                          this.gr_pile.nrm_mat . Mul (cm_above.nmat),
+                          cm_above.ipmat . Mul (this.gr_pile.inv_pnt_mat),
+                          cm_above.inmat . Mul (this.gr_pile.inv_nrm_mat));
+      return this.cumu_mats;
+    }
+
+  Inhale (ratch, thyme)
+    { if (this.gr_pile != null)
+        this.gr_pile . Inhale (ratch, thyme);
+      return 0;
     }
 }
