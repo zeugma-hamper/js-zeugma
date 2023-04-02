e0918c9ae4996be7ff13432f2ed0eb573bb8e28f
Borton Fweems <borton.fweems@treadleandloam.com>
2023-04-02 13:48:55 -0700

    in zeugma-lib.js, convert paths from 'absolute' to 'relative'.

--- zeugma-lib.js
+++ zeugma-lib.js
@@ -35,57 +35,60 @@ zeu.base_class = base_class;
 ///
 //
 
-import { Vect } from "/modules/zeugma/Vect.js";
-import { Matrix44 } from "/modules/zeugma/Matrix44.js";
-import { Geom } from "/modules/zeugma/Geom.js";
-import { MotherTime } from "/modules/zeugma/MotherTime.js";
-import { ZeColor } from "/modules/zeugma/ZeColor.js";
+import { Vect } from "./Vect.js";
+import { Matrix44 } from "./Matrix44.js";
+import { Geom } from "./Geom.js";
+import { MotherTime } from "./MotherTime.js";
+import { ZeColor } from "./ZeColor.js";
 zeu.Vect = Vect;
 zeu.Matrix44 = Matrix44;
 zeu.Geom = Geom;
 zeu.MotherTime = MotherTime;
 zeu.ZeColor = ZeColor;
 
-import { ZeColl } from "/modules/zeugma/ZeColl.js";
-import { ZeWeakColl } from "/modules/zeugma/ZeWeakColl.js";
+import { ZeColl } from "./ZeColl.js";
+import { ZeWeakColl } from "./ZeWeakColl.js";
 zeu.ZeColl = ZeColl;
 zeu.ZeWeakColl = ZeWeakColl;
 
-import { Zeubject } from "/modules/zeugma/Zeubject.js";
-import { IContainMultitudes } from "/modules/zeugma/IContainMultitudes.js";
-import { PouchyThing } from "/modules/zeugma/PouchyThing.js";
+import { Zeubject } from "./Zeubject.js";
+import { IContainMultitudes } from "./IContainMultitudes.js";
+import { PouchyThing } from "./PouchyThing.js";
 zeu.Zeubject = Zeubject;
 zeu.IContainMultitudes = IContainMultitudes;
 zeu.PouchyThing = PouchyThing;
 
-import { IronLung } from "/modules/zeugma/IronLung.js";
-import { CommsSump } from "/modules/zeugma/CommsSump.js";
-import { OSCSump } from "/modules/zeugma/OSCSump.js";
-import { OSCViveWandSump } from "/modules/zeugma/OSCViveWandSump.js";
-import { ViveWandEventSynth } from "/modules/zeugma/ViveWandEventSynth.js";
-import { EventAqueduct } from "/modules/zeugma/EventAqueduct.js";
+import { IronLung } from "./IronLung.js";
+import { CommsSump } from "./CommsSump.js";
+import { OSCSump } from "./OSCSump.js";
+import { OSCViveWandSump } from "./OSCViveWandSump.js";
+import { ViveWandEventSynth } from "./ViveWandEventSynth.js";
+import { EventAqueduct } from "./EventAqueduct.js";
 zeu.IronLung = IronLung;
 zeu.CommsSump = CommsSump;
+zeu.OSCSump = OSCSump;
+zeu.OSCViveWandSump = OSCViveWandSump;
+zeu.ViveWandEventSynth = ViveWandEventSynth;
 zeu.EventAqueduct = EventAqueduct;
 
-import { OSCFaker } from "/modules/zeugma/OSCFaker.js"
-import { CheapOSCMessage } from "/modules/zeugma/CheapOSCMessage.js"
+import { OSCFaker } from "./OSCFaker.js"
+import { CheapOSCMessage } from "./CheapOSCMessage.js"
 zeu.OSCFaker = OSCFaker;
 zeu.CheapOSCMessage = CheapOSCMessage;
 
-import { Loopervisor } from "/modules/zeugma/Loopervisor.js";
+import { Loopervisor } from "./Loopervisor.js";
 zeu.Loopervisor = Loopervisor;
 
-import { Zoft } from "/modules/zeugma/Zoft.js";
-import { LatchZoft } from "/modules/zeugma/LatchZoft.js";
-import { SumZoft } from "/modules/zeugma/SumZoft.js";
-import { DiffZoft } from "/modules/zeugma/DiffZoft.js";
-import { ProdZoft } from "/modules/zeugma/ProdZoft.js";
-import { ScaleZoft } from "/modules/zeugma/ScaleZoft.js";
-import { LoopZoft } from "/modules/zeugma/LoopZoft.js";
-import { SinuZoft } from "/modules/zeugma/SinuZoft.js";
-import { InterpZoft } from "/modules/zeugma/InterpZoft.js";
-import { ZoftInterpFuncs } from "/modules/zeugma/ZoftInterpFuncs.js";
+import { Zoft } from "./Zoft.js";
+import { LatchZoft } from "./LatchZoft.js";
+import { SumZoft } from "./SumZoft.js";
+import { DiffZoft } from "./DiffZoft.js";
+import { ProdZoft } from "./ProdZoft.js";
+import { ScaleZoft } from "./ScaleZoft.js";
+import { LoopZoft } from "./LoopZoft.js";
+import { SinuZoft } from "./SinuZoft.js";
+import { InterpZoft } from "./InterpZoft.js";
+import { ZoftInterpFuncs } from "./ZoftInterpFuncs.js";
 zeu.Zoft = Zoft;
 zeu.LatchZoft = LatchZoft;
 zeu.SumZoft = SumZoft;
@@ -97,13 +100,13 @@ zeu.SinuZoft = SinuZoft;
 zeu.InterpZoft = InterpZoft;
 zeu.ZoftInterpFuncs = ZoftInterpFuncs;
 
-import { CumuMats } from "/modules/zeugma/CumuMats.js";
-import { Grappler } from "/modules/zeugma/Grappler.js";
-import { CoGrappler } from "/modules/zeugma/CoGrappler.js";
-import { TrGrappler } from "/modules/zeugma/TrGrappler.js";
-import { ScGrappler } from "/modules/zeugma/ScGrappler.js";
-import { RoGrappler } from "/modules/zeugma/RoGrappler.js";
-import { GrapplerPile } from "/modules/zeugma/GrapplerPile.js";
+import { CumuMats } from "./CumuMats.js";
+import { Grappler } from "./Grappler.js";
+import { CoGrappler } from "./CoGrappler.js";
+import { TrGrappler } from "./TrGrappler.js";
+import { ScGrappler } from "./ScGrappler.js";
+import { RoGrappler } from "./RoGrappler.js";
+import { GrapplerPile } from "./GrapplerPile.js";
 zeu.CumuMats = CumuMats;
 zeu.Grappler = Grappler;
 zeu.CoGrappler = CoGrappler;
@@ -112,12 +115,12 @@ zeu.ScGrappler = ScGrappler;
 zeu.RoGrappler = RoGrappler;
 zeu.GrapplerPile = GrapplerPile;
 
-import { Limnable } from "/modules/zeugma/Limnable.js";
-import { LimnyThing } from "/modules/zeugma/LimnyThing.js";
-import { SpaceThing } from "/modules/zeugma/SpaceThing.js";
-import { Alignifer } from "/modules/zeugma/Alignifer.js";
-import { PolygonPile } from "/modules/zeugma/PolygonPile.js";
-import { ImageSplatter } from "/modules/zeugma/ImageSplatter.js";
+import { Limnable } from "./Limnable.js";
+import { LimnyThing } from "./LimnyThing.js";
+import { SpaceThing } from "./SpaceThing.js";
+import { Alignifer } from "./Alignifer.js";
+import { PolygonPile } from "./PolygonPile.js";
+import { ImageSplatter } from "./ImageSplatter.js";
 zeu.Limnable = Limnable;
 zeu.LimnyThing = LimnyThing;
 zeu.SpaceThing = SpaceThing;
@@ -125,20 +128,20 @@ zeu.Alignifer = Alignifer;
 zeu.PolygonPile = PolygonPile;
 zeu.ImageSplatter = ImageSplatter;
 
-import { PlatonicMaes } from "/modules/zeugma/PlatonicMaes.js";
-import { Bolex } from "/modules/zeugma/Bolex.js";
+import { PlatonicMaes } from "./PlatonicMaes.js";
+import { Bolex } from "./Bolex.js";
 zeu.PlatonicMaes = PlatonicMaes;
 zeu.Bolex = Bolex;
 
-import { ZeEvent } from "/modules/zeugma/ZeEvent.js"
-import { ZESpatialEvent } from "/modules/zeugma/ZESpatialEvent.js"
-import { ZESpatialAppearEvent } from "/modules/zeugma/ZESpatialAppearEvent.js";
-import { ZESpatialVanishEvent } from "/modules/zeugma/ZESpatialVanishEvent.js";
-import { ZESpatialMoveEvent } from "/modules/zeugma/ZESpatialMoveEvent.js";
-import { ZESpatialCaressEvent } from "/modules/zeugma/ZESpatialCaressEvent.js";
-import { ZESpatialPressureEvent } from "/modules/zeugma/ZESpatialPressureEvent.js";
-import { ZESpatialHardenEvent } from "/modules/zeugma/ZESpatialHardenEvent.js";
-import { ZESpatialSoftenEvent } from "/modules/zeugma/ZESpatialSoftenEvent.js";
+import { ZeEvent } from "./ZeEvent.js"
+import { ZESpatialEvent } from "./ZESpatialEvent.js"
+import { ZESpatialAppearEvent } from "./ZESpatialAppearEvent.js";
+import { ZESpatialVanishEvent } from "./ZESpatialVanishEvent.js";
+import { ZESpatialMoveEvent } from "./ZESpatialMoveEvent.js";
+import { ZESpatialCaressEvent } from "./ZESpatialCaressEvent.js";
+import { ZESpatialPressureEvent } from "./ZESpatialPressureEvent.js";
+import { ZESpatialHardenEvent } from "./ZESpatialHardenEvent.js";
+import { ZESpatialSoftenEvent } from "./ZESpatialSoftenEvent.js";
 zeu.ZeEvent = ZeEvent;
 zeu.ZESpatialEvent = ZESpatialEvent
 zeu.ZESpatialAppearEvent = ZESpatialAppearEvent;
@@ -149,13 +152,13 @@ zeu.ZESpatialPressureEvent = ZESpatialPressureEvent;
 zeu.ZESpatialHardenEvent = ZESpatialHardenEvent;
 zeu.ZESpatialSoftenEvent = ZESpatialSoftenEvent;
 
-import { ZESpatialPhagy } from "/modules/zeugma/ZESpatialPhagy.js";
+import { ZESpatialPhagy } from "./ZESpatialPhagy.js";
 zeu.ZESpatialPhagy = ZESpatialPhagy;
 
-import { Cursoresque } from "/modules/zeugma/Cursoresque.js";
+import { Cursoresque } from "./Cursoresque.js";
 zeu.Cursoresque = Cursoresque;
 
-import { ZeWholeShebang } from "/modules/zeugma/ZeWholeShebang.js";
+import { ZeWholeShebang } from "./ZeWholeShebang.js";
 zeu.ZeWholeShebang = ZeWholeShebang;
 
 
