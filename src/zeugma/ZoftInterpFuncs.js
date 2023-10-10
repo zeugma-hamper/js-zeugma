749d42c9acd1731c5bd34f74a62cccc4954f330a
Borton Fweems <borton.fweems@treadleandloam.com>
2023-10-09 17:25:03 -0700

    good grief... this ain't your daddy's this (fixing ZoftInterpFuncs).

--- src/zeugma/ZoftInterpFuncs.js
+++ src/zeugma/ZoftInterpFuncs.js
@@ -15,17 +15,25 @@ export class ZoftInterpFuncs
 
   static ASYMP_A (t)  { return Math.exp (-7.62462 * (1.0 - t)); }
   static ASYMP_B (t)  { return (1.0 - Math.exp (-7.62462 * t)); }
-  static ASYMP_AB (t)  { return this.JANUSIZE (t, this.ASYMP_A, this.ASYMP_B); }
-  static ASYMP (t)  { return this.ASYMP_B (t); }
+  static ASYMP_AB (t)
+    { return ZoftInterpFuncs.JANUSIZE (t, ZoftInterpFuncs.ASYMP_A,
+                                       ZoftInterpFuncs.ASYMP_B);
+    }
+  static ASYMP (t)  { return ZoftInterpFuncs.ASYMP_B (t); }
 
   static QUADRATIC_A (t)  { return (t * t); }
   static QUADRATIC_B (t)  { return (t * (2.0 - t)); }
   static QUADRATIC_AB (t)
-    { return this.JANUSIZE (t, this.QUADRATIC_A, this.QUADRATIC_B); }
-  static QUADRATIC (t)  { return this.QUADRATIC_B (t); }
+    { return ZoftInterpFuncs.JANUSIZE (t, ZoftInterpFuncs.QUADRATIC_A,
+                                       ZoftInterpFuncs.QUADRATIC_B);
+    }
+  static QUADRATIC (t)  { return ZoftInterpFuncs.QUADRATIC_B (t); }
 
   static CUBIC_A (t)  { return (t * t * t); }
   static CUBIC_B (t)  { return (t * (3.0 - (t * (3.0 - t)))); }
-  static CUBIC_AB (t)  { return this.JANUSIZE (t, this.CUBIC_A, this.CUBIC_B); }
-  static CUBIC (t)  { return this.CUBIC_B (t); }
+  static CUBIC_AB (t)
+    { return ZoftInterpFuncs.JANUSIZE (t, ZoftInterpFuncs.CUBIC_A,
+                                       ZoftInterpFuncs.CUBIC_B);
+    }
+  static CUBIC (t)  { return ZoftInterpFuncs.CUBIC_B (t); }
 }
